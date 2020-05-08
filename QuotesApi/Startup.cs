179b1aa52ed0684a06d4827c33689e2c6bd3d758
ErasylAbenov <abenov.erasyl@gmail.com>
2020-05-08 16:24:26 +0600

    Content Negotiation

--- QuotesApi/Startup.cs
+++ QuotesApi/Startup.cs
@@ -29,6 +29,7 @@ namespace QuotesApi
         {
             services.AddControllers();
             services.AddDbContext<QuotesDbContext>(option => option.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=QuotesDb;"));
+            services.AddMvc().AddXmlSerializerFormatters();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
